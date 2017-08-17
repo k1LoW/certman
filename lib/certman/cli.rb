63690997df734b78690fc9e499f7820225f50ac6
k1LoW <k1lowxb@gmail.com>
2017-08-17 21:36:55 +0900

    Add command `restore-resources`

--- lib/certman/cli.rb
+++ lib/certman/cli.rb
@@ -24,6 +24,27 @@ Receipt Rule Set. OK?"))
       puts ''
     end
 
+    desc 'restore-resources [DOMAIN]', 'Restore resources to receive approval mail'
+    option :hosted_zone, type: :string
+    def restore_resources(domain)
+      pastel = Pastel.new
+      prompt = TTY::Prompt.new
+      return unless prompt.yes?(pastel.red("NOTICE! Your selected region is *#{Aws.config[:region]}*. \
+Certman create certificate on *#{Aws.config[:region]}*. OK?"))
+      client = Certman::Client.new(domain, options)
+      return unless prompt.yes?(pastel.red("NOTICE! Certman use *#{client.region_by_hash}* S3/SES. OK?"))
+      return unless prompt.yes?(pastel.red("NOTICE! When requesting, Certman apend Receipt Rule to current Active \
+Receipt Rule Set. OK?"))
+      Signal.trap(:INT) do
+        puts ''
+        puts pastel.red('Rollback start.')
+        client.rollback
+      end
+      client.restore_resources
+      puts 'Done.'
+      puts ''
+    end
+
     desc 'delete [DOMAIN]', 'Delete ACM Certificate'
     def delete(domain)
       Certman::Client.new(domain, options).delete
